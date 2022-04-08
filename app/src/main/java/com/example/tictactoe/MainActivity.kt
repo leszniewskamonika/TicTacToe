4671c3f422a22275b6a0f270d43a1cc8a01e4547
Monika Leszniewska <monia.leszniewska@gmail.com>
2022-04-08 22:15:54 +0200

    Check result for 'X' and '0'

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tictactoe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -60,5 +61,68 @@ class MainActivity : AppCompatActivity() {
                 btn.text = "0"
             }
         }
+        win()
     }
+
+    fun win(){
+        //horizontal
+        if(b1.text == "X" && b2.text == "X" && b3.text == "X"){
+            toast("X wygrywa grę")
+        }
+        if(b1.text == "0" && b2.text == "0" && b3.text == "0"){
+            toast("0 wygrywa grę")
+        }
+        if(b4.text == "X" && b5.text == "X" && b6.text == "X"){
+            toast("X wygrywa grę")
+        }
+        if(b4.text == "0" && b5.text == "0" && b6.text == "0"){
+            toast("0 wygrywa grę")
+        }
+        if(b7.text == "X" && b8.text == "X" && b9.text == "X"){
+            toast("X wygrywa grę")
+        }
+        if(b7.text == "0" && b8.text == "0" && b9.text == "0"){
+            toast("0 wygrywa grę")
+        }
+
+        //obliquely
+        if(b1.text == "X" && b5.text == "X" && b9.text == "X"){
+            toast("X wygrywa grę")
+        }
+        if(b1.text == "0" && b5.text == "0" && b9.text == "0"){
+            toast("0 wygrywa grę")
+        }
+        if(b3.text == "X" && b5.text == "X" && b7.text == "X"){
+            toast("X wygrywa grę")
+        }
+        if(b3.text == "0" && b5.text == "0" && b7.text == "0"){
+            toast("0 wygrywa grę")
+        }
+
+        //vertical
+        if(b1.text == "X" && b4.text == "X" && b7.text == "X"){
+            toast("X wygrywa grę")
+        }
+        if(b1.text == "0" && b4.text == "0" && b7.text == "0"){
+            toast("0 wygrywa grę")
+        }
+        if(b2.text == "X" && b5.text == "X" && b8.text == "X"){
+            toast("X wygrywa grę")
+        }
+        if(b2.text == "0" && b5.text == "0" && b8.text == "0"){
+            toast("0 wygrywa grę")
+        }
+        if(b3.text == "X" && b6.text == "X" && b9.text == "X"){
+            toast("X wygrywa grę")
+        }
+        if(b3.text == "0" && b6.text == "0" && b9.text == "0"){
+            toast("0 wygrywa grę")
+        }
+
+    }
+
+    fun toast(message: String){
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+    }
+
 }
