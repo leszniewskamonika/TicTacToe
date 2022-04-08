37ceae53d78ab9830cf80f8e63ac1f05ce4204e3
Monika Leszniewska <monia.leszniewska@gmail.com>
2022-04-08 23:30:32 +0200

    Add match draw and minimization if loop

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -70,62 +71,41 @@ class MainActivity : AppCompatActivity() {
 
     fun win(){
         //horizontal
-        if(b1.text == "X" && b2.text == "X" && b3.text == "X"){
-            toast("X wygrywa grę")
-        }
-        if(b1.text == "0" && b2.text == "0" && b3.text == "0"){
-            toast("0 wygrywa grę")
-        }
-        if(b4.text == "X" && b5.text == "X" && b6.text == "X"){
-            toast("X wygrywa grę")
-        }
-        if(b4.text == "0" && b5.text == "0" && b6.text == "0"){
-            toast("0 wygrywa grę")
-        }
-        if(b7.text == "X" && b8.text == "X" && b9.text == "X"){
-            toast("X wygrywa grę")
-        }
-        if(b7.text == "0" && b8.text == "0" && b9.text == "0"){
-            toast("0 wygrywa grę")
-        }
+        if(b1.text == "X" && b2.text == "X" && b3.text == "X"
+            || b4.text == "X" && b5.text == "X" && b6.text == "X"
+            || b7.text == "X" && b8.text == "X" && b9.text == "X"
+            || b1.text == "X" && b5.text == "X" && b9.text == "X"
+            || b3.text == "X" && b5.text == "X" && b7.text == "X"
+            || b1.text == "X" && b4.text == "X" && b7.text == "X"
+            || b2.text == "X" && b5.text == "X" && b8.text == "X"
+            || b3.text == "X" && b6.text == "X" && b9.text == "X"){
 
-        //obliquely
-        if(b1.text == "X" && b5.text == "X" && b9.text == "X"){
-            toast("X wygrywa grę")
+            textResult.text = "X wygrał grę :)"
+            disableButtons()
         }
-        if(b1.text == "0" && b5.text == "0" && b9.text == "0"){
-            toast("0 wygrywa grę")
-        }
-        if(b3.text == "X" && b5.text == "X" && b7.text == "X"){
-            toast("X wygrywa grę")
-        }
-        if(b3.text == "0" && b5.text == "0" && b7.text == "0"){
-            toast("0 wygrywa grę")
-        }
+        else  if(b1.text == "0" && b2.text == "0" && b3.text == "0"
+            || b4.text == "0" && b5.text == "0" && b6.text == "0"
+            || b7.text == "0" && b8.text == "0" && b9.text == "0"
+            || b1.text == "0" && b5.text == "0" && b9.text == "0"
+            || b3.text == "0" && b5.text == "0" && b7.text == "0"
+            || b1.text == "0" && b4.text == "0" && b7.text == "0"
+            || b2.text == "0" && b5.text == "0" && b8.text == "0"
+            || b3.text == "0" && b6.text == "0" && b9.text == "0"){
 
-        //vertical
-        if(b1.text == "X" && b4.text == "X" && b7.text == "X"){
-            toast("X wygrywa grę")
+            textResult.text = "0 wygrał grę :)"
+            disableButtons()
         }
-        if(b1.text == "0" && b4.text == "0" && b7.text == "0"){
-            toast("0 wygrywa grę")
+        else {
+         if(b1.text != "" && b2.text != "" && b3.text != ""
+             && b4.text != "" && b5.text != "" && b6.text != ""
+             && b7.text != "" && b8.text != "" && b9.text != ""){
+             textResult.text = "Remis :D"
+         }
         }
-        if(b2.text == "X" && b5.text == "X" && b8.text == "X"){
-            toast("X wygrywa grę")
-        }
-        if(b2.text == "0" && b5.text == "0" && b8.text == "0"){
-            toast("0 wygrywa grę")
-        }
-        if(b3.text == "X" && b6.text == "X" && b9.text == "X"){
-            toast("X wygrywa grę")
-        }
-        if(b3.text == "0" && b6.text == "0" && b9.text == "0"){
-            toast("0 wygrywa grę")
-        }
-
     }
 
     fun toast(message: String){
+
         Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
     }
 
@@ -139,6 +119,28 @@ class MainActivity : AppCompatActivity() {
         b7.text = ""
         b8.text = ""
         b9.text = ""
+
+        b1.isEnabled = true
+        b2.isEnabled = true
+        b3.isEnabled = true
+        b4.isEnabled = true
+        b5.isEnabled = true
+        b6.isEnabled = true
+        b7.isEnabled = true
+        b8.isEnabled = true
+        b9.isEnabled = true
+    }
+
+    fun disableButtons(){
+        b1.isEnabled = false
+        b2.isEnabled = false
+        b3.isEnabled = false
+        b4.isEnabled = false
+        b5.isEnabled = false
+        b6.isEnabled = false
+        b7.isEnabled = false
+        b8.isEnabled = false
+        b9.isEnabled = false
     }
 
 }
