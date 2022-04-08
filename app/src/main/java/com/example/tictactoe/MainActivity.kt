099f9212ba15f3057dec977d71373893d07bcead
Monika Leszniewska <monia.leszniewska@gmail.com>
2022-04-08 19:24:12 +0200

    Create button click for two players

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,10 +2,63 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var player = "p1"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        b1.setOnClickListener {
+         buttonClick(b1)
+        }
+
+        b2.setOnClickListener {
+            buttonClick(b2)
+        }
+
+        b3.setOnClickListener {
+            buttonClick(b3)
+        }
+
+        b4.setOnClickListener {
+            buttonClick(b4)
+        }
+
+        b5.setOnClickListener {
+            buttonClick(b5)
+        }
+
+        b6.setOnClickListener {
+            buttonClick(b6)
+        }
+
+        b7.setOnClickListener {
+            buttonClick(b7)
+        }
+
+        b8.setOnClickListener {
+            buttonClick(b8)
+        }
+
+        b9.setOnClickListener {
+            buttonClick(b9)
+        }
+    }
+
+    fun buttonClick(btn: Button){
+        if(btn.text == ""){
+            if(player == "p1") {
+                player = "p2"
+                btn.text = "X"
+            } else {
+                player = "p1"
+                btn.text = "0"
+            }
+        }
     }
 }
